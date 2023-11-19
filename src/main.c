fb943c277444a07e0a36c8e306ca1ad8a9fa0fa8
Capronii <gcapronii@hotmail.com>
2023-11-19 02:24:33 -0300

    a

--- src/main.c
+++ src/main.c
@@ -8,12 +8,28 @@
 #include "lvgl.h"
 #include "touch/touch.h"
 
+
 /************************************************************************/
 /* LCD / LVGL                                                           */
 /************************************************************************/
 
+LV_FONT_DECLARE(dseg70);
+LV_FONT_DECLARE(seg30);
+LV_FONT_DECLARE(seg40);
+
 #define LV_HOR_RES_MAX          (320)
 #define LV_VER_RES_MAX          (240)
+
+typedef struct  {
+  uint32_t year;
+  uint32_t month;
+  uint32_t day;
+  uint32_t week;
+  uint32_t hour;
+  uint32_t minute;
+  uint32_t second;
+} calendar;
+
 
 /*A static or global variable to store the buffers*/
 static lv_disp_draw_buf_t disp_buf;
@@ -49,11 +65,100 @@ extern void vApplicationMallocFailedHook(void) {
 	configASSERT( ( volatile void * ) NULL );
 }
 
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
+
+static  lv_obj_t * labelBtn1;
+static  lv_obj_t * labelBtn2;
+static  lv_obj_t * labelBtn3;
+static  lv_obj_t * labelBtn4;
+static  lv_obj_t * labelBtn5;
+static  lv_obj_t * labelBtn6;
+static  lv_obj_t * labelBtn7;
+static  lv_obj_t * labelBtn8;
+
+lv_obj_t * labelFloor;
+lv_obj_t * labelFloor2;
+lv_obj_t * labelSetValue;
+lv_obj_t * labelClock;
+// lv_obj_t * labelHome;
+
+
+xSemaphoreHandle xSemaphoreAtualiza;
+
+volatile int flag_ligar = 0;
+volatile int render = 0;
+volatile int hora = 0;
+
+volatile int current_hour, current_min, current_sec;  
+/************************************************************************/
+/* rtc                                                                  */
+/************************************************************************/
+
+int flag_rtc_alarm = 0;
+
+void RTC_Handler(void) {
+    uint32_t ul_status = rtc_get_status(RTC);
+	
+    /* seccond tick */
+    if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {	
+	// o código para irq de segundo vem aqui
+		BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    	xSemaphoreGiveFromISR(xSemaphoreAtualiza, &xHigherPriorityTaskWoken);
+    }
+	
+    /* Time or date alarm */
+    if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+    	// o código para irq de alame vem aqui
+        flag_rtc_alarm = 1;
+    }
+
+    rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+    rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+    rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+    rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+    rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+    rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+}
+
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type) {
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(rtc, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(rtc, t.year, t.month, t.day, t.week);
+	rtc_set_time(rtc, t.hour, t.minute, t.second);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(id_rtc);
+	NVIC_ClearPendingIRQ(id_rtc);
+	NVIC_SetPriority(id_rtc, 4);
+	NVIC_EnableIRQ(id_rtc);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(rtc,  irq_type);
+}
+
 /************************************************************************/
 /* lvgl                                                                 */
 /************************************************************************/
 
-static void event_handler(lv_event_t * e) {
+static void ligar_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+		flag_ligar = !flag_ligar;
+		render = 0;
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+static void menu_handler(lv_event_t * e) {
 	lv_event_code_t code = lv_event_get_code(e);
 
 	if(code == LV_EVENT_CLICKED) {
@@ -64,26 +169,225 @@ static void event_handler(lv_event_t * e) {
 	}
 }
 
-void lv_ex_btn_1(void) {
-	lv_obj_t * label;
+static void clock_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
 
-	lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
-	lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-	lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
 
-	label = lv_label_create(btn1);
-	lv_label_set_text(label, "Corsi");
-	lv_obj_center(label);
+static void down_handler(lv_event_t * e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    char *c;
+    int temp;
+    if(code == LV_EVENT_CLICKED) {
+		c = lv_label_get_text(labelSetValue);
+		temp = atoi(c);
+		lv_label_set_text_fmt(labelSetValue, "%02d", temp - 1);
+	}
+    
+}
 
+static void up_handler(lv_event_t * e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    char *c;
+    int temp;
+    if(code == LV_EVENT_CLICKED) {
+        c = lv_label_get_text(labelSetValue);
+        temp = atoi(c);
+        lv_label_set_text_fmt(labelSetValue, "%02d", temp + 1);
+    }
+	
+}
+
+static void home_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+static void setting_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+static void heat_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+void lv_ligar(void){
+	static lv_style_t style;
+	lv_style_init(&style);
+	lv_style_set_bg_color(&style, lv_color_black());
+
+	/* Botao ligar */
+    lv_obj_t * labelBtn1;
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+    lv_obj_add_event_cb(btn1, ligar_handler, LV_EVENT_ALL, NULL);
+	// lv_obj_set_width(btn1, 60);  lv_obj_set_height(btn1, 60);
+    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_style(btn1, &style, 0);
+
+    labelBtn1 = lv_label_create(btn1);
+	lv_label_set_text(labelBtn1, LV_SYMBOL_POWER);
+	lv_obj_center(labelBtn1);
+}
+
+void lv_termostato(void) {
+	/* Create a default screen */
+	static lv_style_t style;
+	lv_style_init(&style);
+	lv_style_set_bg_color(&style, lv_color_black());
+	// lv_style_set_border_color(&style, lv_palette_main(LV_PALETTE_GREEN));
+	// lv_style_set_border_width(&style, 5);
+
+
+	/* Botao ligar */
+    lv_obj_t * labelBtn1;
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+    lv_obj_add_event_cb(btn1, ligar_handler, LV_EVENT_ALL, NULL);
+	// lv_obj_set_width(btn1, 60);  lv_obj_set_height(btn1, 60);
+    lv_obj_align(btn1, LV_ALIGN_BOTTOM_LEFT, 0, -20);
+	lv_obj_add_style(btn1, &style, 0);
+
+    labelBtn1 = lv_label_create(btn1);
+	lv_label_set_text(labelBtn1, "[  " LV_SYMBOL_POWER);
+	lv_obj_center(labelBtn1);
+
+	/* Botao menu */
+	lv_obj_t * labelBtn2;
 	lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
-	lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-	lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-	lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-	lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+	lv_obj_add_event_cb(btn2, menu_handler, LV_EVENT_ALL, NULL);
+	// lv_obj_set_width(btn2, 60);  lv_obj_set_height(btn2, 60);
+	lv_obj_align_to(btn2, btn1, LV_ALIGN_TOP_RIGHT, 35, -10);
+	lv_obj_add_style(btn2, &style, 0);
 
-	label = lv_label_create(btn2);
-	lv_label_set_text(label, "Toggle");
-	lv_obj_center(label);
+	labelBtn2 = lv_label_create(btn2);
+	lv_label_set_text(labelBtn2, "| M |");
+	lv_obj_center(labelBtn2);
+
+	/* Botao clock */
+	lv_obj_t * labelBtn3;
+	lv_obj_t * btn3 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn3, clock_handler, LV_EVENT_ALL, NULL);
+	// lv_obj_set_width(btn3, 60);  lv_obj_set_height(btn3, 60);
+	lv_obj_align_to(btn3, btn2, LV_ALIGN_TOP_RIGHT, 35, -7);
+	lv_obj_add_style(btn3, &style, 0);
+
+	labelBtn3 = lv_label_create(btn3);
+	lv_label_set_text(labelBtn3, LV_SYMBOL_SETTINGS "  ]");
+	lv_obj_center(labelBtn3);
+
+	// /* Botao seta para baixo*/
+	lv_obj_t * labelBtn4;
+	lv_obj_t * btn4 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn4, down_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align(btn4, LV_ALIGN_BOTTOM_RIGHT, 0,-21);
+	lv_obj_add_style(btn4, &style, 0);
+
+	labelBtn4 = lv_label_create(btn4);
+	lv_label_set_text(labelBtn4, LV_SYMBOL_DOWN "  ]");
+	lv_obj_center(labelBtn4);
+
+	/* Botao seta para cima*/
+	lv_obj_t * labelBtn5;
+	lv_obj_t * btn5 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn5, up_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn5, btn4, LV_ALIGN_TOP_LEFT, -80, -8);
+	lv_obj_add_style(btn5, &style, 0);
+
+	labelBtn5 = lv_label_create(btn5);
+	lv_label_set_text(labelBtn5, "[  " LV_SYMBOL_UP);
+	lv_obj_center(labelBtn5);	
+
+	/* Botao Home */
+	lv_obj_t * labelBtn6;
+	lv_obj_t * btn6 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn6, home_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn6, btn5, LV_ALIGN_TOP_LEFT, -50, -40);
+	lv_obj_add_style(btn6, &style, 0);
+
+	labelBtn6 = lv_label_create(btn6);
+	lv_label_set_text(labelBtn6, LV_SYMBOL_HOME);
+	lv_obj_center(labelBtn6);
+
+
+	/* LABELS */
+
+	// labelHome = lv_label_create(lv_scr_act());
+	// lv_obj_align_to(labelHome, btn5, LV_ALIGN_TOP_LEFT, -80, -20);
+	// lv_obj_set_style_text_font(labelHome, &seg30, LV_STATE_DEFAULT);
+	// lv_obj_set_style_text_color(labelHome, lv_color_white(), LV_STATE_DEFAULT);
+	// lv_label_set_text_fmt(labelHome, LV_SYMBOL_HOME);
+
+	labelFloor = lv_label_create(lv_scr_act());
+    lv_obj_align(labelFloor, LV_ALIGN_LEFT_MID, 15 , -35);
+    lv_obj_set_style_text_font(labelFloor, &dseg70, LV_STATE_DEFAULT);
+    lv_obj_set_style_text_color(labelFloor, lv_color_white(), LV_STATE_DEFAULT);
+    lv_label_set_text_fmt(labelFloor, "%02d", 23);
+
+	labelFloor2 = lv_label_create(lv_scr_act());
+	lv_obj_align_to(labelFloor2, labelFloor, LV_ALIGN_OUT_RIGHT_BOTTOM, 0 , -2);
+	lv_obj_set_style_text_font(labelFloor2, &seg30, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelFloor2, lv_color_white(), LV_STATE_DEFAULT);
+	lv_label_set_text_fmt(labelFloor2, ".%01d", 4);
+
+	labelSetValue = lv_label_create(lv_scr_act());
+	lv_obj_align(labelSetValue, LV_ALIGN_RIGHT_MID, -20 , -50);
+	lv_obj_set_style_text_font(labelSetValue, &seg40, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelSetValue, lv_color_white(), LV_STATE_DEFAULT);
+	lv_label_set_text_fmt(labelSetValue, "%02d", 22);
+
+	labelClock = lv_label_create(lv_scr_act());
+	lv_obj_align(labelClock, LV_ALIGN_TOP_RIGHT, 0 , 0);
+	lv_obj_set_style_text_font(labelClock, &seg30, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelClock, lv_color_white(), LV_STATE_DEFAULT);
+	// lv_label_set_text_fmt(labelClock, "%02d:%02d", 17, 46);
+
+	/* Botao CLOCK2 */
+	lv_obj_t * labelBtn7;
+	lv_obj_t * btn7 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn7, setting_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn7, labelSetValue, LV_ALIGN_BOTTOM_LEFT, -5, 30);
+	lv_obj_add_style(btn7, &style, 0);
+
+	labelBtn7 = lv_label_create(btn7);
+	lv_label_set_text(labelBtn7, LV_SYMBOL_SETTINGS);
+	lv_obj_center(labelBtn7);
+	
+
+	/* Botao Heat */
+	lv_obj_t * labelBtn8;
+	lv_obj_t * btn8 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn8, heat_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn8, labelSetValue, LV_ALIGN_BOTTOM_RIGHT, -5, 30);
+	lv_obj_add_style(btn8, &style, 0);
+
+	labelBtn8 = lv_label_create(btn8);
+	lv_label_set_text(labelBtn8, LV_SYMBOL_WIFI);
+	lv_obj_center(labelBtn8);
 }
 
 /************************************************************************/
@@ -92,13 +396,47 @@ void lv_ex_btn_1(void) {
 
 static void task_lcd(void *pvParameters) {
 	int px, py;
-
-	lv_ex_btn_1();
+	// lv_termostato();
 
 	for (;;)  {
+
+		if(flag_ligar && !render){
+			//limpar o display	
+			lv_obj_clean(lv_scr_act());	
+			//criar o displaylv_termostato();
+			lv_termostato();
+			render = 1;
+		}
+		else if (!flag_ligar && !render){
+			//limpar o display	
+			lv_obj_clean(lv_scr_act());	
+			//criar o display
+			lv_ligar();	
+			render = 1;
+		}
+
 		lv_tick_inc(50);
 		lv_task_handler();
 		vTaskDelay(50);
+	}
+}
+
+static void task_atualiza(void *pvParameters) {
+	calendar rtc_initial = {2018, 3, 19, 12, 12, 59 ,50};                                         
+    RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_SECEN);
+	int flag = 0;
+	for (;;)  {
+		if(xSemaphoreTake(xSemaphoreAtualiza, 0)) {
+ 			rtc_get_time(RTC, &current_hour, &current_min, &current_sec);
+			if(flag){
+				lv_label_set_text_fmt(labelClock, "%02d:%02d", current_hour, current_min);
+				flag = 0;
+			}
+			else{
+				lv_label_set_text_fmt(labelClock, "%02d %02d", current_hour, current_min);
+				flag = 1;	
+			}
+		}
 	}
 }
 
@@ -193,11 +531,22 @@ int main(void) {
 	configure_touch();
 	configure_lvgl();
 
+
 	/* Create task to control oled */
 	if (xTaskCreate(task_lcd, "LCD", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create lcd task\r\n");
 	}
-	
+	if(xTaskCreate(task_atualiza, "Atualiza", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create atualiza task\r\n");
+	}
+	xSemaphoreAtualiza = xSemaphoreCreateBinary();
+	if(xSemaphoreAtualiza == NULL) {
+		xSemaphoreAtualiza = xSemaphoreCreateBinary();
+	}
+	LV_FONT_DECLARE(dseg70);
+	LV_FONT_DECLARE(seg40);
+	LV_FONT_DECLARE(seg30);
+
 	/* Start the scheduler. */
 	vTaskStartScheduler();
 
